ae62a6f0c62d65fcf593f2291a1ddd9e4f2faad4
zhengpj <598359997@qq.com>
2025-04-25 23:27:29 +0800

    登录开始游戏界面优化

--- src/modules/mahjong/view/MahjongHomeMdr.ts
+++ src/modules/mahjong/view/MahjongHomeMdr.ts
@@ -1,21 +1,20 @@
 import { ui } from "@ui/layaMaxUI";
 import { MahjongProxy } from "../model/MahjongProxy";
-import Button = Laya.Button;
 import Scene = Laya.Scene;
-import Handler = Laya.Handler;
+import Image = Laya.Image;
 
 /**
  * @author zpj
  * @date 2025/1/21
  */
 export default class MahjongHomeMdr extends ui.modules.mahjong.MahjongHomeUI {
-  private _btnStart: Button;
+  private _btnStart: Image;
 
   public createChildren(): void {
     super.createChildren();
     MahjongProxy.ins().model.init();
-    this._btnStart = <Button>this.getChildByName("btnStart");
-    this._btnStart.clickHandler = Handler.create(this, this.onClickBtnStart, undefined, true);
+    this._btnStart = <Image>this.getChildByName("btnStart");
+    this._btnStart.on(Laya.Event.CLICK, this, this.onClickBtnStart);
   }
 
   public onOpened(param: any): void {
@@ -25,6 +24,7 @@ export default class MahjongHomeMdr extends ui.modules.mahjong.MahjongHomeUI {
 
   public onClosed(type?: string): void {
     super.onClosed(type);
+    this._btnStart.offAll(Laya.Event.CLICK);
   }
 
   private onClickBtnStart(): void {
